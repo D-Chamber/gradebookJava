2dac1376486975e16003e4646262f14d7e7eccc3
D-Chamber <timothynnguyen95@gmail.com>
2021-05-06 10:24:22 -0400

    changed teacher,java

--- gradebookProject/Teacher.java
+++ gradebookProject/Teacher.java
@@ -1,25 +1,41 @@
-
 import java.util.Scanner;
-public class Teacher{
+import java.util.*;
+public class Teacher
+{
+public Teacher() {
+      Scanner n = new Scanner(System.in);
+      String name; //variables
+      double grade[]; //variables  
+      int students; //variables 
+      
+    
+      System.out.println("Enter student's name:"); // the name of the student in class
+      name = n.nextLine();   
+      
+       
+      System.out.println("Student's name is: " + name); //the printing of student's name 
+      
 
-    public Teacher() {
-        System.out.println("Enter the number of grades that are going to be calculated of that subject: "); //input 
-        Scanner scanner = new Scanner(System.in);
-        int cla = scanner.nextInt();
-        double[] arr = new double[cla]; //Declaring array of elements
-         
-        double total = 0;
+      System.out.print("Enter the number of grades that are going to be calculated of that subject: "); //the number of grades the teacher user wants to grade of whichever subject 
+      Scanner sc = new Scanner(System.in);
+      students = sc.nextInt(); //scans through the input number of grades that will be calculated
+      grade = new double[students]; //declaring array determined by the user input 
 
-        for(int i=0; i<arr.length; i++){
-        	System.out.print("Enter in grade #" +(i+1)+" : "); //input of the grade
-        	arr[i] = scanner.nextDouble();
-        }
-        scanner.close();
-        for(int i=0; i<arr.length; i++){ 
-        	total = total + arr[i];
-        }
-        double average = total / arr.length; //finding the total average
-        
-        System.out.format("The average is: %.3f", average); //print from input
+      System.out.println();
+      
+
+      for(int i = 0; i < students; i++){ //for loop array
+          System.out.print("Enter in grade #" +(i+1)+" : "); // the grades that would be inputted by user
+          grade[i] = sc.nextDouble(); //takes the input of the user 
+          }
+      double sum = 0; //calculate sum of array
+      for(int i = 0; i < students; i++){
+          sum = sum + grade[i]; //takes the sum and input to get the average
+           }
+
+        double average = sum/students; //calculate the total average
+
+        System.out.println("The average is : "+ average);//printing the average of the student 
+
     }
 }
